a166ba413840ed7ed56af1697393068e62d50039
Robert_Lopez <rplopez720@gmail.com>
2021-10-29 17:00:49 -0400

    Health app services rebuild

---
+++ HealthApp.Services/PrimaryTableFoodService.cs
@@ -0,0 +1,126 @@
+﻿using HealthApp.Models.PrimaryTableFood;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HealthApp.WebMVC.Models.ApplicationUser;
+
+namespace HealthApp.Services
+{
+    public class PrimaryTableFoodService
+    {
+        private readonly Guid _userId;
+        public PrimaryTableFoodService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePrimaryFoodTable(FoodCreate model)
+        {
+            var entity =
+                new FoodCreate()
+                {
+                    FoodId = model.FoodId,
+                    FoodName = model.FoodName,
+                    TotalCaloriesConsumed = model.TotalCaloriesConsumed,
+                    FoodContent = model.FoodContent,
+                    DailyWeight = model.DailyWeight,
+                    CupsWaterDrank = model.CupsWaterDrank,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.FitnessTables.Add(entity); //I dont understand why this logic doesnt work. The var entity contains all of the data in the create class to create 1 instance.
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<FoodListItem> GetPrimaryTableFood()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .FoodTables
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                        new FoodListItem
+                        {
+                            FoodId = e.FoodId,
+                            FoodName = e.FoodName,
+                            TotalCaloriesConsumed = e.TotalCaloriesConsumed,
+                            FoodContent = e.FoodContent,
+                            DailyWeight = e.DailyWeight,
+                            CupsWaterDrank = e.CupsWaterDrank,
+                            CreatedUtc = e.CreatedUtc,
+                            ModifiedUtc = e.ModifiedUtc
+                        }
+                    );
+                return query.ToArray();
+            }
+        }
+
+        //Not quite sure what the error here relates to 
+        public GetPrimaryTableFoodById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .FoodTables
+                    .Single(e => e.FoodId == id && e.OwnerId == _userId);
+
+                return
+                    new FoodDetail
+                    {
+                        OwnerId = entity.OwnerId,
+                        FoodId = entity.FoodId,
+                        FoodName = entity.FoodName,
+                        TotalCaloriesConsumed = entity.TotalCaloriesConsumed,
+                        FoodContent = entity.FoodContent,
+                        DailyWeight = entity.DailyWeight,
+                        CupsWaterDrank = entity.CupsWaterDrank,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc,
+                    };
+            }
+        }
+
+        public bool UpdatePrimaryTableFood(FoodEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .FoodTables
+                        .Single(e => e.FoodId == model.FoodId && e.OwnerId == _userId);
+
+                entity.FoodId = model.FoodId;
+                entity.FoodName = model.FoodName;
+                entity.TotalCaloriesConsumed = model.TotalCaloriesConsumed;
+                entity.FoodContent = model.FoodContent;
+                entity.DailyWeight = model.DailyWeight;
+                entity.CupsWaterDrank = model.CupsWaterDrank;
+                entity.CreatedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public bool PrimaryTableFoodDelete(int MyMealPlan)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .FoodTables
+                    .Single(e => e.FoodId == MyMealPlan && e.OwnerId == _userId);
+                ctx.FoodTables.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
---
+++ HealthApp.Services/PrimaryTableSpiritService.cs
@@ -0,0 +1,132 @@
+﻿using HealthApp.Models.PrimaryTableFitness;
+using HealthApp.Models.PrimaryTableSpirit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HealthApp.WebMVC.Models.ApplicationUser;
+
+namespace HealthApp.Services
+{
+    public class PrimaryTableSpiritService
+    {
+        private readonly Guid _userId;
+
+        public PrimaryTableSpiritService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateSpiritTable(SpiritCreate model)
+        {
+            var entity =
+                new SpiritCreate()
+                {
+                    HowIViewMe = model.HowIViewMe,
+                    HowIViewOthers = model.HowIViewOthers,
+                    HowOtherPerceiveMe = model.HowOtherPerceiveMe,
+                    MySocialCircle = model.MySocialCircle,
+                    MyRestHours = model.MyRestHours,
+                    OutsideMotivation = model.OutsideMotivation,
+                    InternalMotivaiton = model.InternalMotivaiton,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.SpiritTables.Add(entity); //same reason as the primary fitness table;
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<SpiritListItem> GetPrimarySpiritTable()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .SpiritTables
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                        new SpiritListItem
+                        {
+                            HowIViewMe = e.HowIViewMe,
+                            HowIViewOthers = e.HowIViewOthers,
+                            HowOtherPerceiveMe = e.HowOtherPerceiveMe,
+                            MySocialCircle = e.MySocialCircle,
+                            MyRestHours = e.MyRestHours,
+                            OutsideMotivation = e.OutsideMotivation,
+                            InternalMotivaiton = e.InternalMotivaiton,
+                            CreatedUtc = e.CreatedUtc,
+                            ModifiedUtc = e.ModifiedUtc
+                        }
+                  );
+                return query.ToArray();
+            }
+        }
+
+        public GetPrimarySpiritTableById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .SpiritTables
+                    .Single(e => e.HowIViewMe == id && e.OwnerId == _userId);
+
+                return
+                    new PrimaryTableSpirit
+                    {
+                        HowIViewMe = entity.HowIViewMe,
+                        HowIViewOthers = entity.HowIViewOthers,
+                        HowOtherPerceiveMe = entity.HowOtherPerceiveMe,
+                        MySocialCircle = entity.MySocialCircle,
+                        MyRestHours = entity.MyRestHours,
+                        OutsideMotivation = entity.OutsideMotivation,
+                        InternalMotivaiton = entity.InternalMotivaiton,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc,
+                    };
+            }
+        }
+
+        public bool UpdateSpiritTable
+            (SpiritEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .SpiritTables
+                    .Single(e => e.HowIViewMe == model.HowIViewMe && e.OwnerId == _userId);
+
+                entity.HowIViewMe = model.HowIViewMe;
+                entity.HowIViewOthers = model.HowIViewOthers;
+                entity.HowOtherPerceiveMe = model.HowOtherPerceiveMe;
+                entity.MySocialCircle = model.MySocialCircle;
+                entity.MyRestHours = model.MyRestHours;
+                entity.OutsideMotivation = model.OutsideMotivation;
+                entity.InternalMotivaiton = model.InternalMotivaiton;
+                entity.CreatedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool PrimaryTableSpiritDelete(int MySpiritPlan)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .SpiritTables
+                    .Single(e => e.HowIViewMe == MySpiritPlan && e.OwnerId == _userId);
+                ctx.SpiritTables.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
