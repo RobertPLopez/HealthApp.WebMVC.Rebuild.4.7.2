d04553d3665900d595e74a0767678f75321af78a
Robert_Lopez <rplopez720@gmail.com>
2021-10-29 17:04:52 -0400

    Spirit and Fitness Controller rebuild

---
+++ HealthApp.WebMVC/Controllers/PrimaryTableFoodController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthApp.WebMVC.Controllers
+{
+    public class PrimaryTableFoodController : Controller
+    {
+        // GET: PrimaryTableFood
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
