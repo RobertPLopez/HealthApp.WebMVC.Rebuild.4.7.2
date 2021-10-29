5f897d6a9c8a197fb6910b871bba0b4196ce2917
Robert_Lopez <rplopez720@gmail.com>
2021-10-29 16:34:53 -0400

    Health data rebuild

---
+++ HealthApp.Data/PrimaryTableFitness.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthApp.Data
+{
+    public class PrimaryTableFitness
+    {
+        [Key]
+        public int WorkoutId { get; set; }
+        [ForeignKey("Profile Number")]
+        public Guid OwnerId { get; set; }
+        //[Required]
+        //public ICollection <Excersises> : ExcersiseTabele I need to be able to connect this to the sub fitness tables 
+        [Required]
+        public int TotalCaloriesBurned { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
